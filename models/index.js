10b83daa473122af2a5fee3cb65c418ec698364c
Arooba Shahoor <ashahoor@rekd-app.com>
2021-10-13 11:06:55 +0500

    Update models-index to define field names underscore config

--- models/index.js
+++ models/index.js
@@ -11,6 +11,12 @@ let sequelize;
 if (config.use_env_variable) {
 	sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {
+	config.define = {
+		createdAt: 'created_at',
+		updatedAt: 'updated_at',
+		deletedAt: 'deleted_at'
+	};
+
 	sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
