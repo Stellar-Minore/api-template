bb83b4005223261951ca8a381e0234d9076aca68
Huzaifa Aneel <haneel@stellarminore.com>
2023-01-16 09:46:00 +0500

    Create route for swagger docs

---
+++ routes/docs.js
@@ -0,0 +1,73 @@
+const express = require('express');
+const swaggerJsdoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
+const swaggerDefinition = require('../docs/swaggerDef');
+
+const router = express.Router();
+
+const specs = swaggerJsdoc({
+	swaggerDefinition,
+	apis: ['docs/*.yml', 'routes/*.js'],
+});
+
+router.use('/', swaggerUi.serve);
+router.get('/',
+	swaggerUi.setup(specs, {
+		explorer: true
+	}));
+
+/**
+ * @swagger
+ * tags:
+ *   name: Index
+ */
+
+/**
+ * @swagger
+ * /signup:
+ *   post:
+ *     summary: Register a user.
+ *     tags: [Index]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *              $ref: '#/components/schemas/User'
+ *     responses:
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ */
+
+/**
+ * @swagger
+ * /login:
+ *   get:
+ *     summary: Login.
+ *     tags: [Index]
+ *     parameters:
+ *       - in: header
+ *         name: email
+ *         schema:
+ *           type: string
+ *           format: email
+ *         required: true
+ *       - in: header
+ *         name: pw
+ *         schema:
+ *           type: string
+ *         required: true
+ *     responses:
+ *       "200":
+ *         description: Success
+ *         content:
+ *           application/json:
+ *             schema:
+ *                $ref: '#/components/schemas/User'
+ */
+
+module.exports = router;
