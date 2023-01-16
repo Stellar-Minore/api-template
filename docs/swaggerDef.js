4fbbefefaba4d36090acbf2a053bab3d40af9981
Huzaifa Aneel <haneel@stellarminore.com>
2023-01-16 09:45:21 +0500

    Add swagger definition in swagger docs

---
+++ docs/swaggerDef.js
@@ -0,0 +1,13 @@
+const swaggerDef = {
+	openapi: '3.0.0',
+	info: {
+		title: 'Stellar Minore Api Template'
+	},
+	servers: [
+		{
+			url: 'http://localhost:3000',
+		},
+	]
+};
+
+module.exports = swaggerDef;
