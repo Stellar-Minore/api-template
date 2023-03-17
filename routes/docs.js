45ed20c3d83581f5e2422f9df9defff68ae22fa1
Huzaifa Aneel <haneel@stellarminore.com>
2023-03-17 09:20:22 +0500

    Remove swagger routes from doc.js

--- routes/docs.js
+++ routes/docs.js
@@ -7,7 +7,7 @@ const router = express.Router();
 
 const specs = swaggerJsdoc({
 	swaggerDefinition,
-	apis: ['docs/*.yml', 'routes/*.js'],
+	apis: ['docs/*.yml']
 });
 
 router.use('/', swaggerUi.serve);
@@ -15,96 +15,5 @@ router.get('/',
 	swaggerUi.setup(specs, {
 		explorer: true
 	}));
-
-/**
- * @swagger
- * tags:
- *   name: Index
- */
-
-/**
- * @swagger
- * /signup:
- *   post:
- *     summary: Register a user.
- *     tags: [Index]
- *     requestBody:
- *       required: true
- *       content:
- *         application/json:
- *           schema:
- *              $ref: '#/components/schemas/User'
- *     responses:
- *       "200":
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *                $ref: '#/components/schemas/User'
- */
-
-/**
- * @swagger
- * /login:
- *   get:
- *     summary: Login.
- *     tags: [Index]
- *     parameters:
- *       - in: header
- *         name: email
- *         schema:
- *           type: string
- *           format: email
- *         required: true
- *       - in: header
- *         name: pw
- *         schema:
- *           type: string
- *         required: true
- *     responses:
- *       "200":
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *                $ref: '#/components/schemas/User'
- */
-
-/**
- * @swagger
- * /access_token:
- *   get:
- *     summary: Get access and refresh token.
- *     tags: [Index]
- *     responses:
- *       "200":
- *         description: Success
- *         content:
- *           application/json:
- *             schema:
- *                type: object
- *                properties:
- *                  message:
- *                    type: string
- *                  access_token:
- *                    type: string
- *                  refresh_token:
- *                    type: string
- *                example:
- *                  message: 'Access and refresh token granted successfully!'
- *                  access_token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9'
- *                  refresh_token: 'eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9'
- */
-
-/**
- * @swagger
- * /logout:
- *   get:
- *     summary: Logout
- *     tags: [Index]
- *     responses:
- *       "200":
- *         description: Success
- */
 
 module.exports = router;
