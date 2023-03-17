8e46062d9f8fd8519d540f38e34db62dc01a0555
Huzaifa Aneel <haneel@stellarminore.com>
2023-03-17 10:00:17 +0500

    Fix flow in signup

--- routes/index.js
+++ routes/index.js
@@ -16,52 +16,60 @@ router.get('/', (req, res) => {
 	res.render('index', { title: 'Express' });
 });
 
-router.post('/signup', (req, res, next) => {
-	if (!req.body.email) {
+/* POST signup. */
+router.post('/signup', async (req, res) => {
+	if (!req.body.first_name) {
+		return badRequestHandler(res, 'Error: User first name is not specified!');
+	}
+
+	if (!req.body.last_name) {
+		return badRequestHandler(res, 'Error: User last name is not specified!');
+	}
+
+	if (!req.body.user_email) {
 		return badRequestHandler(res, 'Error: User email is not specified!');
 	}
 
 	if (!req.body.password) {
-		return badRequestHandler(res, 'Error: User password is not specified!');
+		return badRequestHandler(res, 'Error: Password is not specified!');
 	}
 
-	const userEmail = req.body.email.toLowerCase();
-	const userPassword = req.body.password;
+	const userEmail = req.body.user_email.toLowerCase();
+	const hashPassword = await bcrypt.hash(req.body.password, bcrypt.genSaltSync(10));
 
 	User.findOne({ where: { email: userEmail } }).then(
 		(user) => {
 			if (!user) {
-				bcrypt.genSalt(10, (err, salt) => {
-					bcrypt.hash(userPassword, salt, (err, hash) => {
-						if (err) return next(err);
+				User.create({
+					first_name: req.body.first_name,
+					last_name: req.body.last_name,
+					email: userEmail,
+					password: hashPassword
+				}).then(
+					async (newUser) => {
+						const accessTokenPrivateKey = (process.env.accessTokenPrivateKey || config.accessTokenPrivateKey).replace(/\\n/g, '\n');
+						const refreshTokenPrivateKey = (process.env.refreshTokenPrivateKey || config.refreshTokenPrivateKey).replace(/\\n/g, '\n');
+						const accessToken = jwt.sign({ user_id: newUser.id }, accessTokenPrivateKey, { expiresIn: '30m', algorithm: 'RS256' });
+						const refreshToken = jwt.sign({ user_id: newUser.id }, refreshTokenPrivateKey, { expiresIn: '30d', algorithm: 'RS256' });
 
-						User.create({ email: userEmail, password: hash }).then(
-							(new_user) => {
-								const accessTokenPrivateKey = config.accessTokenPrivateKey.replace(/\\n/g, '\n');
-								const refreshTokenPrivateKey = config.refreshTokenPrivateKey.replace(/\\n/g, '\n');
-								const accessToken = jwt.sign({ user_id: new_user.id }, accessTokenPrivateKey, { expiresIn: '30m', algorithm: 'RS256' });
-								const newRefreshToken = jwt.sign({ user_id: new_user.id }, refreshTokenPrivateKey, { expiresIn: '1d', algorithm: 'RS256' });
-
-								return res.json({
-									message: 'User Created!',
-									user: new_user,
-									user_exists: false,
-									access_token: accessToken,
-									refresh_token: newRefreshToken
-								});
-							},
-							(err) => {
-								return serverErrorHandler(res, 'Error: Could not create user in sign up', err, { user_create_failed: true });
-							}
-						);
-					});
-				});
+						return res.json({
+							message: 'User created successfully!',
+							user: newUser,
+							access_token: accessToken,
+							refresh_token: refreshToken,
+							user_exists: false
+						});
+					},
+					(err) => {
+						return serverErrorHandler(res, 'Error: Could not create user in POST sign up', err, { user_create_failed: true });
+					}
+				);
 			} else {
-				return badRequestHandler(res, 'Error: User already exists in sign up', { user_exists: true });
+				return badRequestHandler(res, 'Error: User already exists in POST sign up', { user_exists: true });
 			}
 		},
 		(err) => {
-			return serverErrorHandler(res, 'Error: Failed to fetch user profile in sign up', err, { user_fetch_failed: true });
+			return serverErrorHandler(res, 'Error: Failed to fetch user in POST sign up', err, { user_fetch_failed: true });
 		}
 	);
 });
