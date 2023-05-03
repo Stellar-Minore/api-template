8c398887cc342d21f5f0fd40489a8401548b0437
Muhammad Noman <nakhtar@stellarminore.com>
2023-05-03 13:52:54 +0500

    Update user model to add association for reset password code model

--- models/user.js
+++ models/user.js
@@ -4,13 +4,11 @@ const {
 
 module.exports = (sequelize, DataTypes) => {
 	class user extends Model {
-		/**
-   * Helper method for defining associations.
-   * This method is not a part of Sequelize lifecycle.
-   * The `models/index` file will call this method automatically.
-   */
-		static associate() {
-			// define association here
+		static associate(models) {
+			this.hasOne(models.reset_password_code, {
+				foreignKey: 'user_id',
+				sourceKey: 'id'
+			});
 		}
 	}
 	user.init({
