a0add2fcde32651803f3b555d4e44878debb3c4e
Muhammad Noman Akhtar <nakhtar@stellarminore.com>
2024-03-20 20:53:33 +0500

    Create function to delete expired accounts

--- helpers/globals.js
+++ helpers/globals.js
@@ -1,3 +1,4 @@
+const { Op } = require('sequelize');
 const ResetPasswordCode = require('../models').reset_password_code;
 const User = require('../models').user;
 const UserToken = require('../models').user_token;
@@ -12,3 +13,28 @@ exports.deleteUserAccount = (userID, deletePermanently = false) => {
 	]);
 };
 
+exports.deleteExpiredAccounts = () => {
+	User.findAll({ where: { deletion_interval_in_days: { [Op.ne]: null } }, paranoid: false }).then(
+		(users) => {
+			const currentDate = new Date();
+			const deletePermanently = true;
+
+			users.forEach(async (user) => {
+				const deletionDate = new Date(
+					user.updated_at.getTime() + (user.deletion_interval_in_days * 24 * 60 * 60 * 1000)
+				);
+
+				if (deletionDate <= currentDate) {
+					await this.deleteUserAccount(user.id, deletePermanently).catch(
+						(err) => {
+							console.log('Failed to delete user account in delete expired accounts', err);
+						}
+					);
+				}
+			});
+		},
+		(err) => {
+			console.log('Failed to fetch user accounts in delete expired accounts', err);
+		}
+	);
+};
